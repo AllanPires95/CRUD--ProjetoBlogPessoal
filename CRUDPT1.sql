796cd7e43ee57f3bc775beea1449ebe0b7bc0d51
AllanPires95 <allan_sfc@hotmail.com>
2023-01-27 09:02:20 -0300

    PUSH PUT GET DEL

--- CRUDPT1.sql
+++
@@ -1,9 +0,0 @@
-USE db_blogpessoal;
-
-
-INSERT INTO tb_postagens (data, texto, titulo) 
-VALUES (current_timestamp(), 'Postagem 01', 'Texto da postagem 01');
-INSERT INTO tb_postagens (data, texto, titulo) 
-VALUES (current_timestamp(), 'Postagem 02', 'texto da postagem 02');
-
-SELECT * FROM tb_postagens;
